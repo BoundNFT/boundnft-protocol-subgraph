4f6914ef4a1f37701b56775e2e0f4955adb67f59
Thor Seldon <thorseldon@protonmail.com>
2021-11-17 16:48:04 +0800

    add bnft framework

---
+++ src/mapping/bnft-registry.ts
@@ -0,0 +1,34 @@
+import { BNFTCreated, BNFTUpgraded } from "../../generated/BNFTRegistry/BNFTRegistry";
+
+import { BNFT as BNFTContract } from "../../generated/templates";
+import { IERC721Metadata } from "../../generated/templates/BNFT/IERC721Metadata";
+import { getOrInitBNFT } from "../helpers/initializers";
+
+export function handleBNFTCreated(event: BNFTCreated): void {
+  BNFTContract.create(event.params.bNftProxy);
+
+  let bnft = getOrInitBNFT(event.params.bNftProxy);
+
+  bnft.nftAsset = event.params.nftAsset;
+  bnft.tokenContractImpl = event.params.bNftImpl;
+
+  let bNftContract = IERC721Metadata.bind(event.params.bNftProxy);
+
+  bnft.name = bNftContract.name();
+  bnft.symbol = bNftContract.symbol();
+
+  bnft.save();
+}
+
+export function handleBNFTUpgraded(event: BNFTUpgraded): void {
+  let bnft = getOrInitBNFT(event.params.bNftProxy);
+
+  bnft.tokenContractImpl = event.params.bNftImpl;
+
+  let bNftContract = IERC721Metadata.bind(event.params.bNftProxy);
+
+  bnft.name = bNftContract.name();
+  bnft.symbol = bNftContract.symbol();
+
+  bnft.save();
+}
